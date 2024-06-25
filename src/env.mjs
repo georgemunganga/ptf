c8d35413e48dce5cd7bd6dbfd4a574f0eb2aae51
GeorgeMightyfin <gmunganga@mightyfinance.co.zm>
2024-06-25 19:01:27 +0200

    Resolved

--- src/env.mjs
+++ src/env.mjs
@@ -8,7 +8,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
     NEXTAUTH_SECRET:
-      process.env.NODE_ENV === 'production'
+      process.env.NODE_ENV === 'development'
         ? z.string().min(1)
         : z.string().min(1).optional(),
     NEXTAUTH_URL: z.preprocess(
