af4ea87c0dd9fa89c7eb984b9d20bc22748a3ed2
GeorgeMightyfin <gmunganga@mightyfinance.co.zm>
2024-06-26 06:25:37 +0200

    Resolved

--- src/app/api/auth/[...nextauth]/auth-options.ts
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -32,13 +32,13 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async redirect({ url, baseUrl }) {
-      // const parsedUrl = new URL(url, baseUrl);
-      // if (parsedUrl.searchParams.has('callbackUrl')) {
-      //   return `${baseUrl}${parsedUrl.searchParams.get('callbackUrl')}`;
-      // }
-      // if (parsedUrl.origin === baseUrl) {
-      //   return url;
-      // }
+      const parsedUrl = new URL(url, baseUrl);
+      if (parsedUrl.searchParams.has('callbackUrl')) {
+      return `${baseUrl}${parsedUrl.searchParams.get('callbackUrl')}`;
+      }
+      if (parsedUrl.origin === baseUrl) {
+        return url;
+       }
       return baseUrl;
     },
   },
